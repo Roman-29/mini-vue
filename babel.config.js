3d9b3a0fc72646c0673af401e782967d43598eb1
luojw <luojw@tuyuansu.com.cn>
2022-04-10 17:38:47 +0800

    初始化项目, 集成jest和ts

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: luojw
+ * @Date: 2022-04-10 16:49:27
+ * @LastEditors: luojw
+ * @LastEditTime: 2022-04-10 16:50:32
+ * @Description:
+ */
+
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+};
