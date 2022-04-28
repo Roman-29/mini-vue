d2eac99d0d63307db6c331333643d7b3c7cd7f50
luojw <luojw@tuyuansu.com.cn>
2022-04-28 23:39:09 +0800

    实现 isRef 和 unRef 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -2,7 +2,7 @@
  * @Author: luojw
  * @Date: 2022-04-28 14:32:20
  * @LastEditors: luojw
- * @LastEditTime: 2022-04-28 14:45:14
+ * @LastEditTime: 2022-04-28 23:37:46
  * @Description:
  */
 
@@ -15,6 +15,7 @@ class RefImpl {
   private _rawValue;
   // ref自身的依赖收集
   public deps;
+  public __v_isRef = true;
 
   constructor(value) {
     this._value = convert(value);
@@ -48,4 +49,13 @@ function trackRefValue(ref) {
 
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  // 如果是ref返回value, 否则直接返回
+  return isRef(ref) ? ref.value : ref;
 }
--- src/reactivity/test/ref.spec.ts
+++ src/reactivity/test/ref.spec.ts
@@ -2,12 +2,13 @@
  * @Author: luojw
  * @Date: 2022-04-28 14:32:13
  * @LastEditors: luojw
- * @LastEditTime: 2022-04-28 14:32:14
+ * @LastEditTime: 2022-04-28 23:36:31
  * @Description:
  */
 
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, ref, unRef } from "../ref";
 describe("ref", () => {
   it("happy path", () => {
     const a = ref(1);
@@ -44,5 +45,21 @@ describe("ref", () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
+
+  it("unRef", () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
   });
 });
